0a505a4b9890b5a6f46f501d7d8d381e0e5cccf1
Sergey Stryzhevskyi <stryzhevsky.s@gmail.com>
2015-04-19 22:24:16 +0300

    v0.1.0 init commit

--- dist/js/const.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Sergei on 16.04.15.
- */
-define([], function () {
-    return {
-        SECTIONS: ['hot', 'new', 'top', 'controversial'],
-        COMMENT_ORDER: ['top', 'hot', 'new', 'old', 'best', 'controversial']
-    };
-});
